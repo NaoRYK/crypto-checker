a6ef64dd517c7fab1fe1db28b7e1cff700d43422
NaoPlay RYK <dragonarcher2005@gmail.com>
2023-03-05 00:24:06 -0300

    Adding page function and searching function

--- assets/index.js
+++ assets/index.js
@@ -7,6 +7,34 @@ const searchInput = document.querySelector(".input-search");
 const top100 = document.querySelector(".top-btn");
 const btnContainer = document.querySelector(".pagination");
 
+
+const formatearNumero = (num) =>{
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+}
+
+
+const nFormatter = (num, digits) => {
+    const symbol = [
+      { value: 1, symbol: '' },
+      { value: 1e3, symbol: 'K' },
+      { value: 1e6, symbol: 'M' },
+      { value: 1e9, symbol: 'MM' },
+    ];
+  
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  
+    let item = symbol
+      .slice()
+      .reverse()
+      .find(item => {
+        return num >= item.value;
+      });
+  
+    return item
+      ? (num / item.value).toFixed(digits).replace(rx, '$1') + item.symbol
+      : '0';
+  };
+
 const renderCoins = (coins,current) => {
 
     if(!coins.length){
@@ -26,13 +54,13 @@ const renderCoin = coin =>{
     const {rank,market_cap_usd,name,symbol,price_usd,volume24,percent_change_24h,percent_change_7d} = coin;
     return `
       <tr>
-                  <td>#${rank}</td>
-                  <td class="coin-title">${name} (${symbol})</td>
-                  <td>$${price_usd}</td>
-                  <td>$${market_cap_usd}</td>
-                  <td>$${volume24}</td>
-                  <td><span class="${percent_change_24h < 0 ? 'down': 'up'}">${percent_change_24h}</span></td>
-                  <td><span class="${percent_change_7d < 0 ? 'down': 'up'}">${percent_change_7d}</span></td>
+                  <td class="animate">#${rank}</td>
+                  <td class="animate coin-title">${name} (${symbol})</td>
+                  <td class="animate ">$${price_usd}</td>
+                  <td class="animate ">$${nFormatter(market_cap_usd) }</td>
+                  <td class="animate ">$${nFormatter(volume24)}</td>
+                  <td class="animate "><span class="${percent_change_24h < 0 ? 'down': 'up'}">${percent_change_24h}</span></td>
+                  <td class="animate "><span class="${percent_change_7d < 0 ? 'down': 'up'}">${percent_change_7d}</span></td>
        </tr>
       
       `;
@@ -44,6 +72,27 @@ const resetCount = (coins) =>{
     next = coins.next;
     total = coins.total;
     results = coins.results;
+}
+
+
+//Deshabilitar botones
+
+const disablePreviousBtn = (prev) =>{
+    if(prev ===null){
+        prevBTN.classList.add("disabled")
+    }
+    else{
+        prevBTN.classList.remove("disabled")
+    }
+}
+
+const disableNextBTN = (next,total) =>{
+    if(next ===total){
+        nextBTN.classList.add("disabled")
+    }
+    else{
+        nextBTN.classList.remove("disabled")
+    }
 }
 
 
@@ -62,12 +111,35 @@ const loadCoins = async (e) => {
   }
   pageNumber.innerHTML=current+1;
   renderCoins(results, current);
-
+  disablePreviousBtn(prev);
+  disableNextBTN(next,total);
   //Logica del boton Next
 
   nextBTN.addEventListener("click",e =>{
     e.stopImmediatePropagation();
+    if(next === total) return;
+    prev = current;
+    current +=1;
+    next +=1;
+    renderCoins(results,current)
+    pageNumber.innerHTML=current +1;
+    disablePreviousBtn(prev);
+    disableNextBTN(next,total);
+  });
+
+  prevBTN.addEventListener("click",e =>{
+    e.stopImmediatePropagation();
+    if(prev ===null) return;
+    current -= 1;
+    prev = prev === 0 ? null : prev -1;
+    next -= 1;
+    pageNumber.innerHTML = current +1;
+    renderCoins(results,current);
+    disablePreviousBtn(prev);
+    disableNextBTN(next,total);
   })
+
+
 };
 
 //Funcion inicializadora
@@ -75,4 +147,5 @@ const loadCoins = async (e) => {
 const init = (() => {
   window.addEventListener("DOMContentLoaded", loadCoins);
   form.addEventListener("submit", loadCoins);
+  top100.addEventListener("click",loadCoins)
 })();
